8ef1372033b092f80f4e94fef81dc7cea33d5a64
Luis <luis@replicate.com>
2023-12-26 21:53:29 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# diffusion-motion-transfer Cog model
+
+This is an implementation of [diffusion-motion-transfer](https://github.com/diffusion-motion-transfer/diffusion-motion-transfer?tab=readme-ov-file) as a [Cog](https://github.com/replicate/cog) model.
+
+## Development
+
+Follow the [model pushing guide](https://replicate.com/docs/guides/push-a-model) to push your own model to [Replicate](https://replicate.com).
+
+## Basic Usage
+
+Clone the github repo
+
+    git clone https://github.com/diffusion-motion-transfer/diffusion-motion-transfer.git motion/
+
+Run a prediction
+
+    cog predict -i video=@demo.mp4
+
+## Example
+
+![motorbike](output.gif)
---
+++ predict.py
@@ -0,0 +1,88 @@
+# Prediction interface for Cog ⚙️
+# https://github.com/replicate/cog/blob/main/docs/python.md
+
+from cog import BasePredictor, Input, Path
+import os
+import yaml
+import subprocess
+
+class Predictor(BasePredictor):
+    def setup(self) -> None:
+        """Load the model into memory to make running multiple predictions efficient"""
+        # self.model = torch.load("./weights.pth")
+        os.chdir("motion")
+
+    def predict(
+        self,
+        video: Path = Input(description="Input video"),
+        video_prompt: str = Input(
+            description="Prompt used to describe the input video",
+            default="Amazing quality, masterpiece, A locomotive rides in a forest",
+        ),
+        target_prompt: str = Input(
+            description="Prompt used to describe the output video",
+            default="Amazing quality, masterpiece, A motorbike driving in a forest",
+        ),
+        negative_prompt: str = Input(
+            description="Negative prompt to use for editing",
+            default="bad quality, distortions, unrealistic, distorted image, watermark, signature"
+        ),
+    ) -> Path:
+        """Run a single prediction on the model"""
+        # Clear tmp input & output images folder
+        input_path = "data/input"
+        # os.system("rm -rf " + input_path)
+        os.makedirs(input_path, exist_ok=True)
+
+        # Convert input video to images
+        print("Converting input video to images")
+        command = ["ffmpeg", "-i", str(video), input_path+"/%05d.png"]
+        subprocess.run(command, check=True)
+
+        latent_path = "data/input/ddim_latents"
+        config_data = f"""video_path: {input_path}
+save_dir: {latent_path}
+max_number_of_frames: 24
+
+n_timesteps: 999
+prompt: {video_prompt}
+negative_prompt: ""
+save_ddim_reconstruction: False
+        """
+        with open("configs/config.yaml", "w") as file:
+            file.write(config_data)
+
+        # Run preprocessor script
+        print("Running preprocessor script")
+        command = ["python", "preprocess_video_ddim.py", "--config_path", "configs/config.yaml"]
+        subprocess.run(command)
+
+        output_path = "result/output"
+        # Edit editing script
+        with open('configs/guidance_config.yaml', 'r') as f:
+            config = yaml.safe_load(f)
+        # Change the output_path value
+        config['output_path'] = output_path
+        config['data_path'] = input_path
+        config['latents_path'] = latent_path
+        config['source_prompt'] = video_prompt
+        config['target_prompt'] = target_prompt
+        config['negative_prompt'] = negative_prompt
+        # Write the modified configuration back to the YAML file
+        with open('configs/guidance.yaml', 'w') as f:
+            yaml.dump(config, f)
+
+        # Clear result output folder
+        
+        # os.system("rm -rf result")
+        # Run editing script
+        print("Running editing script")
+        command = ["python", "run.py", "--config_path", "configs/guidance.yaml"]
+        subprocess.run(command)
+
+        output_path = "/src/motion/result/output/result.mp4"
+        # if file exists return path
+        if os.path.isfile(output_path):
+            return Path(output_path)
+        else:
+            raise RuntimeError("Error fetching output video")
